4bbb9e46e6d85604821b2ac1880801938251c5a1
markov199 <99345543+markov199@users.noreply.github.com>
2023-06-26 06:12:29 +0400

    Update Bureaucrat.hpp

--- cpp05/ex02/Bureaucrat.hpp
+++ cpp05/ex02/Bureaucrat.hpp
@@ -42,6 +42,7 @@ class Bureaucrat
 		void incrementGrade();
 		void decrementGrade();
 		void signForm(AForm &form);
+		void executeForm(AForm const &form);
 		class GradeTooHighException: public std::exception
 		{
 			public:
