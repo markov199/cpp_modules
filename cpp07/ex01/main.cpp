2a3314b7a3e3b75b763d887e049bf07bef8e39cb
markov199 <99345543+markov199@users.noreply.github.com>
2023-07-15 15:26:47 +0400

    Update main.cpp

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -13,3 +13,16 @@
 #include<iostream>
 #include"iter.hpp"
 
+
+template <typename T> void increment(T a)
+{
+    a = a + 1;
+}
+
+int main (void)
+{
+    char array[3] = {'a', 'f', 'k'}
+    iter(array, 3, &increment);
+  
+}
+
