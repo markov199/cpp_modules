3ca7a64f7bb63b2af9ea5d83957b00960b4e348d
markov199 <99345543+markov199@users.noreply.github.com>
2023-07-19 21:27:05 +0400

    Update easyfind.hpp

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,4 +1,21 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+template <typename T> T::iterator easyfind(const T &cont, int x)
+{
+  T::iterator itr;
+  itr = find_first_of(cont, x);
+  if (itr == std::npos)
+    std::cout << "NO occurance of " << x << " found\n";
+  return (itr);      
+}
+
+template <typename T> void printElements(const T &input)
+{
+  T::iterator itr;
+  for(itr = t.begin; itr != t.end; itr++)
+    std::cout << *itr << << " ";
+  std::cout << std::endl;
+}
+
 #endif
