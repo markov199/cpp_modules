c1d59b65e8dfad8084f380501b4d79b3fb7db350
markov199 <99345543+markov199@users.noreply.github.com>
2023-07-21 22:41:33 +0400

    Update Span.cpp

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,3 +1,25 @@
-Span::Span()
-{
-}
+
+  
+ Span::Span()
+{}
+
+Span::Span(int size)
+{}
+
+Span::~Span()
+{}
+
+Span::Span(const &copy)
+{}
+
+Span &Span::operator=(const &rhs)
+{}
+
+void Span::addNumber(int num)
+{}
+
+unsigned int Span::shortestSpan(void)
+{}
+
+unsigned int Span::longestSpan(void)
+{}
