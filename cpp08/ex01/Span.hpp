2b123b9b38e78320f512f191fbbca4429341ef12
markov199 <99345543+markov199@users.noreply.github.com>
2023-07-21 22:33:49 +0400

    Update Span.hpp

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,4 +1,20 @@
 #ifndef SPAN_HPP
 define SPAN_HPP
 
+class Span
+{
+  private:
+    unsigned interger N;
+
+  public:
+    Span();
+    Span(int size);
+    ~Span();
+    Span(const &copy);
+    Span &operator=(const &rhs);
+    addNumber(int num);
+    shortestSpan(void);
+    longestSpan(void);
+}
+
 #endif
